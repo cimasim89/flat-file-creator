6cd757543cf28bd71fd1705e6d9f8810ce16b537
Kael Shipman <kael.shipman@cfxtrading.com>
2020-06-26 18:35:19 -0500

    feat: added 'dataToLines' function

--- src/get-async-flat-file-creator.test.ts
+++ src/get-async-flat-file-creator.test.ts
@@ -1,9 +1,17 @@
-import { getAsyncFlatFileCreator as f } from "./get-async-flat-file-creator";
+import {
+  getAsyncFlatFileCreator as f,
+  dataToLines,
+} from './get-async-flat-file-creator'
+import { testFields, testData, testLines } from './TestData'
 
-const getAsyncFlatFileCreator: any = f;
+const getAsyncFlatFileCreator: any = f
 
 describe('makeAsyncFlatFileWriter', () => {
   it('returning a promise function', () => {
     expect(typeof getAsyncFlatFileCreator(null)).toBe('function')
+  })
+
+  it('should serialize data to an array of lines', () => {
+    expect(dataToLines(testData, testFields).join(`\n`)).toEqual(testLines)
   })
 })
