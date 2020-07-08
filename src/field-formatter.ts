e7d529f025587a2903cb01ac2014fb91addf2ad6
Kael Shipman <kael.shipman@cfxtrading.com>
2020-07-08 10:42:09 -0500

    Fixed issue with typing RowData

--- src/field-formatter.ts
+++ src/field-formatter.ts
@@ -1,12 +1,12 @@
-import stringFormatter from "./string-formatter";
-import dateFormatter from "./date-formatter";
-import integerFormatter from "./integer-formatter";
-import floatFormatter from "./float-formatter";
-import { FieldSpec, StringFieldSpec, RowData, assertFieldSpec } from "./Types";
+import stringFormatter from './string-formatter'
+import dateFormatter from './date-formatter'
+import integerFormatter from './integer-formatter'
+import floatFormatter from './float-formatter'
+import { FieldSpec, StringFieldSpec, RowData, assertFieldSpec } from './Types'
 
-const fieldFormatter = (map: FieldSpec, data: RowData) => {
-  assertFieldSpec(map);
-  const fieldName = map.name
+const fieldFormatter = <T>(map: FieldSpec, data: RowData<T>) => {
+  assertFieldSpec(map)
+  const fieldName = <keyof T>map.name
   switch (map.type) {
     case 'string':
       return stringFormatter(map, data[fieldName])
@@ -17,10 +17,13 @@ const fieldFormatter = (map: FieldSpec, data: RowData) => {
     case 'date':
       return dateFormatter(map, data[fieldName])
     case undefined:
-      return stringFormatter({ ...(map as StringFieldSpec), type: "string" }, data[fieldName])
+      return stringFormatter(
+        { ...(map as StringFieldSpec), type: 'string' },
+        data[fieldName]
+      )
     default:
       throw new Error('required field type is not present')
   }
 }
 
-export default fieldFormatter;
+export default fieldFormatter
