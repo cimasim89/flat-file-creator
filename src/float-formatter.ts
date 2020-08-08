41ac810f98cf8459657b88d4f047e6dd324e45c7
Kael Shipman <kael.shipman@gmail.com>
2020-08-08 01:27:01 -0500

    Implemented default values on write (#53)

--- src/float-formatter.ts
+++ src/float-formatter.ts
@@ -6,23 +6,37 @@ import {
   getPadder,
   getFillStringOfSymbol,
 } from './utils'
-import { FloatFieldSpec, FieldValue, assertFieldSpec } from './Types'
+import { FloatFieldSpec, FloatFieldValue, assertFieldSpec } from './Types'
 
 const paddingDefault = 'start'
 
-const floatFormatter = (map: FloatFieldSpec, data: FieldValue = '') => {
+const floatFormatter = (map: FloatFieldSpec, data: FloatFieldValue = null) => {
   assertFieldSpec(map, 'float')
-  if (!isNumeric(data)) {
-    throw new Error(`field [${map.name}] has not compatible type`)
+
+  // If the data is null or undefined and has a default value defined, use that, otherwise set
+  // to incoming data
+  data = data === undefined || data === null ? map.default : data
+
+  let str: string
+
+  if (data === undefined) {
+    throw new Error(`No value supplied and no default set`)
+  } else if (data === null) {
+    str = ''
+  } else {
+    if (!isNumeric(data)) {
+      throw new Error(`field [${map.name}] has not compatible type`)
+    }
+    const precision = !map.precision ? 0 : map.precision
+    const num = data.toFixed(precision + 1)
+    str = !map.dotNotation
+      ? Math.trunc(Number(num) * Math.pow(10, precision)).toString()
+      : data.toFixed(precision).toString()
+    if (_.size(str) > map.size) {
+      throw new Error(`Value ${str} exceed size ${map.size}`)
+    }
   }
-  const precision = !map.precision ? 0 : map.precision
-  const num = data.toFixed(precision + 1)
-  const str = !map.dotNotation
-    ? Math.trunc(Number(num) * Math.pow(10, precision)).toString()
-    : data.toFixed(precision).toString()
-  if (_.size(str) > map.size) {
-    throw new Error(`Value ${str} exceed size ${map.size}`)
-  }
+
   return getPadder(
     getPaddingPositionOrDef(map.paddingPosition, paddingDefault)
   )(
--- src/string-formatter.ts
+++ src/string-formatter.ts
@@ -6,48 +6,58 @@ import {
   getPadder,
   getFillStringOfSymbol,
 } from './utils'
-import { StringFieldSpec, FieldValue, assertFieldSpec } from './Types'
+import { StringFieldSpec, StringFieldValue, assertFieldSpec } from './Types'
 import { FlatFileEnumError } from './Errors'
 
 const paddingDefault = 'end'
 
-const stringFormatter = (map: StringFieldSpec, data: FieldValue = '') => {
+const stringFormatter = (map: StringFieldSpec, data: StringFieldValue) => {
   assertFieldSpec(map, 'string')
   if (map.straight && isNumeric(data)) {
     throw new Error('field has not compatible type')
   }
 
-  // Set initial value
-  let str = data === null ? '' : data.toString()
+  // If the data is null or undefined and has a default value defined, use that, otherwise set
+  // to incoming data
+  data = data === undefined || data === null ? map.default : data
 
-  // If we don't want to preserve white space, trim it now
-  if (map.preserveEmptySpace || map.preserveEmptySpace === undefined) {
-    str = str.trim()
-  }
+  let str: string
 
-  // If this is an enum field, conver the value to serialized form
-  if (data !== null && map.enum !== undefined) {
-    // If we find the string in the values of the enum, convert it to a key
-    let i = Object.values(map.enum).indexOf(str)
-    if (i > -1) {
-      str = Object.keys(map.enum)[i]
-    } else {
-      // Otherwise, check to see if it's among the keys of the enum
-      i = Object.keys(map.enum).indexOf(str)
-      if (i === -1) {
-        // If not, throw an exception
-        throw new FlatFileEnumError(
-          `Value for field '${
+  if (data === undefined) {
+    throw new Error(`No value supplied and no default set`)
+  } else if (data === null) {
+    str = ''
+  } else {
+    str = data
+    // If we don't want to preserve white space, trim it now
+    if (map.preserveEmptySpace || map.preserveEmptySpace === undefined) {
+      str = str.trim()
+    }
+
+    // If this is an enum field, convert the value to serialized form
+    if (data !== null && map.enum !== undefined) {
+      // If we find the string in the values of the enum, convert it to a key
+      let i = Object.values(map.enum).indexOf(str)
+      if (i > -1) {
+        str = Object.keys(map.enum)[i]
+      } else {
+        // Otherwise, check to see if it's among the keys of the enum
+        i = Object.keys(map.enum).indexOf(str)
+        if (i === -1) {
+          // If not, throw an exception
+          throw new FlatFileEnumError(
+            `Value for field '${
+              map.name
+            }' should have been one of the accepted values ["${Object.values(
+              map.enum
+            ).join(`", "`)}"]${
+              typeof map.default !== 'undefined' && map.default === null
+                ? ` or null`
+                : ``
+            }, but you passed '${str}'`,
             map.name
-          }' should have been one of the accepted values ["${Object.values(
-            map.enum
-          ).join(`", "`)}"]${
-            typeof map.default !== 'undefined' && map.default === null
-              ? ` or null`
-              : ``
-          }, but you passed '${str}'`,
-          map.name
-        )
+          )
+        }
       }
     }
   }
