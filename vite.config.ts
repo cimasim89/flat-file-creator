87458d15dbf6e59e84a0a8be1b8f80fb1cf936c8
Diego Carvalho <dcarvalho@engagedmd.com>
2024-02-16 08:03:29 -0300

    ci: add new workflows for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { configDefaults, defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config'
 
 // Change to UTC
-process.env.TZ = "UTC";
+process.env.TZ = 'UTC'
 
 export default defineConfig({
-  test: configDefaults,
-});
+  test: {
+    coverage: {
+      enabled: true,
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'clover', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
+  },
+})
